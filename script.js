eb7506324b1be4257bad6d5c5034fa88d692cb4b
Maria Ilieva <123466748+marianilieva@users.noreply.github.com>
2023-02-11 11:25:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+$(function()    {
+    $('.menu').on('click', function()   {
+        if ($('.menu-tags').css('display') == 'none')   {
+            $('.menu-tags').css('display', 'block');
+        }
+        else    {
+            $('.menu-tags').hide();
+        }
+    });
+
+    $('.ask').on('click', function(event) {
+        event.preventDefault();
+        $.ajax({
+            URL: 'quotes.json?time="+newDate().getTime',
+            type: 'get',
+            contentType: 'application/json',
+            cache: false,
+            headers:    {
+                "Cache-control": "no-cache, no-store, must-revalidate",
+                "Pragma": "no-cache",
+                "Expires": 0
+            },
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error)   {
+                console.log('error');
+            }
+        });
+    });
+});
